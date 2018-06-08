f907ab8c8b3b01d1ac67ab637b7d098e9217ff85
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-08 10:43:03 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -2,10 +2,14 @@ __precompile__()
 
 module CentripetalCatmullRom
 
-export CatmullRom, PT1D, PT2D, PT3D, PT4D
+export CatmullRom, 
+       PT1D, PT2D, PT3D, PT4D, coords,   # points in 1D, 2D, 3D, 4D [coordinates :x, :y, :z, :t]
+       xcoord, ycoord, zcoord, tcoord,   # access point coordinates in 1D, 2D, 3D, 4D
+       Δpoint, Δpoint2, dpoint, dpoint2, # dpoint, dpoint2 alias Δpoint, Δpoint2   
+       polyval, polyder                  # from Polynomials, specialized
 
 using Polynomials
-import Polynomials: polyval
+import Polynomials: polyval, polyder
 
 include("points_1Dto4D.jl")
 
