716b45b63f40389e420262bbcd07ff660f3c00ff
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-08 17:47:55 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -17,33 +17,4 @@ import Polynomials: polyval, polyder
 include("points_1Dto4D.jl")
 include("polys_1Dto4D.jl")
 
-
-
-
-function test()
-    p0 = POINT2D(0.0, 0.0)
-    p1 = POINT2D(1.0, 1.0)
-    p2 = POINT2D(1.5, 1.25)
-    p3 = POINT2D(2.0, 0.0)
-
-    crpoly = CatmullRomPolys(p0, p1, p2, p3)
-
-    # p1 ... n-1 points ... p2
-    
-    n = 4
-    coords = Vector{POINT2D}(undef, n+1)
-    m = inv(n)
-
-    for i=0:n
-       xcoord, ycoord = polyval(crpoly, min(1.0,m*i))
-       coord = POINT2D(xcoord, ycoord)
-       coords[i+1] = coord
-    end
-    return coords
-end
-
-print(test())
-
-
-
-end # module
+end # module CentripetalCatmullRom
