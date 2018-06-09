bd4e1a5ab73cca09cbeb2e1768a04eb866f2f370
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-09 04:28:53 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,31 @@
 using CentripetalCatmullRom
-using Test
+
+if VERSION >= v"0.7-"
+    using Test
+else
+    using Base.Test
+end
 
 @test true
 
 #=
 function test()
-    p0 = POINT2D(0.0, 0.0)
-    p1 = POINT2D(1.0, 1.0)
-    p2 = POINT2D(1.5, 1.25)
-    p3 = POINT2D(2.0, 0.0)
+    p0 = PT2D(0.0, 0.0)
+    p1 = PT2D(1.0, 1.0)
+    p2 = PT2D(1.5, 1.25)
+    p3 = PT2D(2.0, 0.0)
 
-    crpoly = CatmullRomPolys(p0, p1, p2, p3)
+    crcubics = CatmullRomCubics(p0, p1, p2, p3)
 
     # p1 ... n-1 points ... p2
     
     n = 4
-    coords = Vector{POINT2D}(undef, n+1)
+    coords = Vector{PT2D}(undef, n+1)
     m = inv(n)
 
     for i=0:n
-       xcoord, ycoord = polyval(crpoly, min(1.0,m*i))
-       coord = POINT2D(xcoord, ycoord)
+       xcoord, ycoord = polyval(crcubics, min(1.0,m*i))
+       coord = PT2D(xcoord, ycoord)
        coords[i+1] = coord
     end
     return coords
