883b9a33456afbf01df644feb4441e123113d8ba
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-17 05:40:31 -0400

    Update ndpoint.jl

--- src/ndpoint.jl
+++ src/ndpoint.jl
@@ -47,32 +47,22 @@ function setindex(pt::Point{N,T}, values::NTuple{M,T}, idxs::R) where {T,N,M,R<:
 end
 
 
-x1(x::Point{N,T}) where {T,N} = x.coords[1]
-x2(x::Point{N,T}) where {T,N} = x.coords[2]
-x3(x::Point{N,T}) where {T,N} = x.coords[3]
-x4(x::Point{N,T}) where {T,N} = x.coords[4]
-x5(x::Point{N,T}) where {T,N} = x.coords[5]
-x6(x::Point{N,T}) where {T,N} = x.coords[6]
-x7(x::Point{N,T}) where {T,N} = x.coords[7]
-x8(x::Point{N,T}) where {T,N} = x.coords[8]
-x9(x::Point{N,T}) where {T,N} = x.coords[9]
-x10(x::Point{N,T}) where {T,N} = x.coords[10]
-x11(x::Point{N,T}) where {T,N} = x.coords[11]
-x12(x::Point{N,T}) where {T,N} = x.coords[12]
-x13(x::Point{N,T}) where {T,N} = x.coords[13]
-x14(x::Point{N,T}) where {T,N} = x.coords[14]
-x15(x::Point{N,T}) where {T,N} = x.coords[15]
-x16(x::Point{N,T}) where {T,N} = x.coords[16]
-x17(x::Point{N,T}) where {T,N} = x.coords[17]
-x18(x::Point{N,T}) where {T,N} = x.coords[18]
-x19(x::Point{N,T}) where {T,N} = x.coords[19]
-x20(x::Point{N,T}) where {T,N} = x.coords[20]
-x21(x::Point{N,T}) where {T,N} = x.coords[21]
-x22(x::Point{N,T}) where {T,N} = x.coords[22]
-x23(x::Point{N,T}) where {T,N} = x.coords[23]
-x24(x::Point{N,T}) where {T,N} = x.coords[24]
-x25(x::Point{N,T}) where {T,N} = x.coords[25]
-x26(x::Point{N,T}) where {T,N} = x.coords[26]
+#=
+    x1(x::Point{N,T}) where {T,N} = x.coords[1]
+    x2(x::Point{N,T}) where {T,N} = x.coords[2]
+    x3(x::Point{N,T}) where {T,N} = x.coords[3]
+=#
+
+macro pointcoords(num)
+    fname = Symbol("x" * string(num))
+    quote
+        $(esc(fname))(pt::Point{N,T}) where {T,N} = pt.coords[$num]
+    end
+end
+
+for i in 1:26
+    @eval @pointcoords($i)
+end
 
 
 # coordinate, squared distance from origin
