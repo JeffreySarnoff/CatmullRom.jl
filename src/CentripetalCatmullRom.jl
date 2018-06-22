50e28428d64c6be60b27c67dc919aee9b2156183
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-22 10:39:26 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -10,13 +10,12 @@ using Polynomials
 import Polynomials: polyval, polyder
 
 """
-    into01(xs...)
     into01((xs...,))
     into01([xs...,])
 
 maps values into 0.0:1.0 (minimum(xs) --> 0.0, maximum(xs) --> 1.0)
 """
-function into01(values::Union{Vararg{T}, Tuple{Vararg{T}, Vector{T}}) where {T<:Real}
+function into01(values::U) where {N,T, U<:Union{NTuple{N,T}, Vector{T}}}
     mn, mx = minimum(values), maximum(values)
     delta = mx - mn
     map(clamp01, (values .- mn) ./ delta)
