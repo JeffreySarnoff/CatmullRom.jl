a299714487a8957f5dc6661470b3fefaec2eb788
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-22 09:57:22 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -19,7 +19,7 @@ maps values into 0.0:1.0 (minimum(xs) --> 0.0, maximum(xs) --> 1.0)
 function into01(values::Union{Vararg{T}, Tuple{Vararg{T}, Vector{T}}) where {T<:Real}
     mn, mx = minimum(values), maximum(values)
     delta = mx - mn
-    map(clamp01, (values .- mn) ./ delta))
+    map(clamp01, (values .- mn) ./ delta)
 end
 
 @inline clamp01(x::T) where {T<:Real} = clamp(x, zero(T), one(T))
