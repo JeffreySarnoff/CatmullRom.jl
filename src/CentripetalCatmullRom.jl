8c3629df6249c1d6fda6e2ef45f8a6325990e669
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-22 09:55:53 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -11,13 +11,18 @@ import Polynomials: polyval, polyder
 
 @inline clamp01(x::T) where {T<:Real} = clamp(x, zero(T), one(T))
 
+"""
+    into01(xs...)
+    into01((xs...,))
+    into01([xs...,])
+
+maps values into 0.0:1.0 (minimum(xs) --> 0.0, maximum(xs) --> 1.0)
+"""
 function into01(values::Union{Vararg{T}, Tuple{Vararg{T}, Vector{T}}) where {T<:Real}
     mn, mx = minimum(values), maximum(values)
     delta = mx - mn
     map(clamp01, (values .- mn) ./ delta))
 end
-
-
 
 
 qrtrroot(x) = sqrt(sqrt(x))
