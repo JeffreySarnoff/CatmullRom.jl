f92678f1229ea31e06e7b8b9ca55a08cbca79a47
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-23 13:58:08 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -108,6 +108,9 @@ function catmullrom_points(pta::T, pt0::T, pt1::T, ptb::T, interpolants::NTuple{
     return points
 end
 
+catmullrom_points1(pta::T, pt0::T, pt1::T, ptb::T, interpolants::NTuple{M,F}) where {N, M, F, T<:NTuple{N,F}} =
+    catmullrom_points(pta, pt0, pt1, ptb, interpolants)[1:end-1,:]
+
 #=
     interpolants
     1 2 3 4 5 6         1 2 3 4 5 6
@@ -147,23 +150,16 @@ function catmullrom(points::U1, interpolants::U2) where {U1, U2}
     totalpoints = (point_spans - 2) * (ninterp - 1) + ninterp
     
     result = Array{eltyp, ndimens}(undef, (totalpoints, ndimens) )
-    interpolants1 = interpolants[1:end-1] #view(interpolants,1:(ninterp-1))
     
-    result[1:(ninterp-1),:] = catmullrom_points(points[1:4]..., interpolants1)
-    result[(1*ninterp):(2*(ninterp-1)),:] = catmullrom_points(points[2:2+3]..., interpolants1)
-    result[(2*(ninterp-1)+1):(3*(ninterp-1)),:] = catmullrom_points(points[3:3+3]..., interpolants1)
-    result[(3*(ninterp-1)+1):(4*(ninterp-1)),:] = catmullrom_points(points[4:4+3]..., interpolants1)
-    result[(4*(ninterp-1)+1):(5*(ninterp-1)+1),:] = catmullrom_points(points[5:5+3]..., interpolants)
+    result[1:(1*(ninterp-1)),:] = catmullrom_points1(points[1:4]..., interpolants)
     
-    #result[(5*(ninterp-1)+1):(6*(ninterp-1)+1),:] = catmullrom_points(points[6:6+3]..., interpolants)
-    
-    #=
-    for i in 2:(point_windows-1)
-        result[(i*(ninterp-1)):((i+1)*(ninterp-1)-1),:] = catmullrom_points(points[i:i+3]..., interpolants1)
+    for i in 1:point_spans-3
+        k = i+1
+        result[(i*(ninterp-1)+1):(k*(ninterp-1)),:] = catmullrom_points1(points[k:k+3]..., interpolants)
     end
-    =#
-    #result[point_windows:(point_windows+ninterp-1), :] = catmullrom_points(points[end-3:end]..., interpolants)
-
+    i = point_spans-2; k = i+1        
+    result[(i*(ninterp-1)+1):(k*(ninterp-1)+1),:] = catmullrom_points(points[k:k+3]..., interpolants)
+    
     return result
 end
 
