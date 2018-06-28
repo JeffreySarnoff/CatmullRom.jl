beab801109e7b1013138bdd624addcc8b2ff6afc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-28 13:38:35 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -9,22 +9,11 @@ import Polynomials: polyval, polyder
 
 using LinearAlgebra: dot
 
+include("interpolant.jl")
+
+
 dpolyval(ply::Poly, value::T) where {T} = polyval(polyder(ply), value)
 
-
-"""
-    into01((xs...,))
-    into01([xs...,])
-
-maps values into 0.0:1.0 (minimum(xs) --> 0.0, maximum(xs) --> 1.0)
-"""
-function into01(values::U) where {N,T, U<:Union{NTuple{N,T}, Vector{T}}}
-    mn, mx = minimum(values), maximum(values)
-    delta = mx - mn
-    map(clamp01, (values .- mn) ./ delta)
-end
-
-@inline clamp01(x::T) where {T<:Real} = clamp(x, zero(T), one(T))
 
 qrtrroot(x) = sqrt(sqrt(x))
 
