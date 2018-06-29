9367b7c55743d96244df6bbc5a06f7a4cb81cc9e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-29 02:49:20 -0400

    Update interpolant.jl

--- src/interpolant.jl
+++ src/interpolant.jl
@@ -6,7 +6,7 @@ maps values into 0.0:1.0 (minimum(xs) --> 0.0, maximum(xs) --> 1.0)
 """
 function into01(values::U) where {N,T, U<:Union{NTuple{N,T}, Vector{T}}}
     mn, mx = minimum(values), maximum(values)
-    delta = mx - mn
+    delta = mx .- mn
     map(clamp01, (values .- mn) ./ delta)
 end
 
