c2516f49baf4986e2d97ef31d0149e8e74a588c0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-29 06:09:12 -0400

    Update centripetal.jl

--- src/centripetal.jl
+++ src/centripetal.jl
@@ -4,4 +4,16 @@
     
 =#
 
+# lineal distance between two points, each point given in Cartesian coordinates
+distance_lineal(point₀, point₁) = sqrt(sum(sqr.(point₁ .- point₀)))
+
+# centripetal adjustment to the distance between two points, each point given in Cartesian coordinates
+distance_centripetal(point₀, point₁) = sqrt(distance_lineal(point₀, point₁))
+
+distances_centripetal(points) = [distance_centripetal(points[i], points[i+1]) for i=1:(length(points)-1)]
+
+totaldistance_centripetal(points) = sum(distances_centripetal(points))
+
+
+centripetal_pullback(points) = [0.0, (cumsum(distances_centripetal(points)) ./ totaldistance_centripetal(points))...,]
 
