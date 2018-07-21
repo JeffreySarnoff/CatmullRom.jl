f9d53a609bf0a2d33db5d60663a86ab9e7eee922
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-21 12:12:51 -0400

    Update interpolant.jl

--- src/interpolant.jl
+++ src/interpolant.jl
@@ -18,32 +18,18 @@ end
 # uniform separation in 0..1 inclusive
 uniformsep(n::Int) = n >= 2 ? collect(0.0:inv(n-1):1.0) : throw(DomainError("$n < 2"))
 
-# Chebyshev type 1 roots mapped into 0..1
-chebroot1_01(k::Int, n::Int) = (1 + cospi( (2*(n-k)+1) / (2*n) )) / 2
-chebroots1_01(n::Int) = n >= 1 ? [chebroot1_01(k,n) for k=1:n] : throw(DomainError("$n < 1"))
+# roots of the Chebyshev polynomials of the second kind, mapped into 0..1
 
 chebroot2_01(k::Int, n::Int) = (1-cospi((k-1)/(n-1)))/2
 chebroots2_01(n::Int) = n >= 1 ? [chebroot2_01(k,n) for k=1:n] : throw(DomainError("$n < 1"))
 
-# Chebyshev type 1 roots mapped into 0..1 with 0 and 1 appended
-"""
-    chebyshev1sep(n)
-
-
-Chebyshev type 1 roots mapped into 0..1, with 0 and 1 appended
-
-chebyshev1sep(5) gives five values: [0.0, a, b, c, 1.0]
-"""
-chebyshev1sep(n::Int) = n >= 0 ? [0.0, chebroots1_01(n-2)..., 1.0] : throw(DomainError("$n < 0"))
-
-
 # Chebyshev type 2 roots mapped into 0..1 with 0 and 1 included
 """
-    chebyshev2sep(n)
+    chebyshevsep(n)
 
 
 Chebyshev type 2 roots mapped into 0..1, with 0 and 1 included
 
-chebyshev2sep(5) gives five values: [0.0, b, c, d, 1.0]
+chebyshevsep(5) gives five values: [0.0, b, c, d, 1.0]
 """
-chebyshev2sep(n::Int) = n >= 0 ? [0.0, chebroots2_01(n-2)..., 1.0] : throw(DomainError("$n < 0"))
+chebyshevsep(n::Int) = n >= 0 ? [0.0, chebroots2_01(n-2)..., 1.0] : throw(DomainError("$n < 0"))
