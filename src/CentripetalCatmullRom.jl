b5d1b0596e245653a1c7c26e1ef63ff2e7cc646b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-22 08:54:52 -0400

    Update CentripetalCatmullRom.jl

--- src/CentripetalCatmullRom.jl
+++ src/CentripetalCatmullRom.jl
@@ -3,22 +3,20 @@ __precompile__()
 module CentripetalCatmullRom
 
 export catmullrom,                      # points, interpolants --> points, interpolated points
-       catmullrom_onpath,               # rough and inexact pathlengths (arc traversals)
+
+       catmullrom_extents,              # rough and inexact pathlengths (arc traversals)
                                         #    covering curvilinear segments between points 
-       uniformsep,
-       chebyshevsep,
-       into01,
-       clamp01,
-       catmullrom_polys,                 # Catmull-Rom centripetal cubics
-       catmullrom_polys_d01,             #     cubics and quadratic first derivatives
-       catmullrom_polys_d012,            #     cubics and quadratic first and monic second derivatives
-       catmullrom_polys_i01,             #     cubics and quadrics of integration
-       Poly, polyval, polyder, polyint   # reexported
+       uniformsep,                      # gen your interpolant values for uniform spacing 
+       chebyshevsep,                    # gen your interpolant values for chebyshev spacing
+
+       Poly, polyval, polyder, polyint  # reexported
 
 using Polynomials
 import Polynomials: Poly, polyval, polyder, polyint
 
 import LinearAlgebra: dot, norm
+
+
 
 include("catmullrom.jl")
 include("interpolant.jl")
