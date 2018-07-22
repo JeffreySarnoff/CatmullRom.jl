e496efd1194a088ceefe98906bf43b4da13540e9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-22 07:38:56 -0400

    Update catmullrom.jl

--- src/catmullrom.jl
+++ src/catmullrom.jl
@@ -24,7 +24,7 @@ catmullrom(points::AbstractArray{T, N}, interpolants::AbstractArray{F, 1}) where
 
 
 
-# ref https://ideone.com/NoEbVM
+# some of the following is adapted from https://ideone.com/NoEbVM
 
 # compute Catmull-Rom cubic curve over [0, 1]
 function catmullrom_cubic(x0::T, x1::T, x2::T, x3::T, dt0::T, dt1::T, dt2::T) where {T}
@@ -63,7 +63,7 @@ end
    interpolating from p1 to p2 inclusive
    one poly for each coordinate axis
 =#
-function ccr_polys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
+function catmullrom_polys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
     dt0, dt1, dt2 = prep_centripetal_catmullrom(pts)
     pt0, pt1, pt2, pt3 = pts
     
@@ -76,117 +76,7 @@ function ccr_polys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
     return polys
 end
 
-function ccr_polys_dpolys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
-    polys = ccr_polys(pts)
-    differentiatepolys = polyder.(polys)
-    return polys, differentiatepolys
-end
 
-function ccr_polys_ipolys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
-    polys = ccr_polys(pts)
-    integratepolys = polyint.(polys)
-    return polys, integratepolys
-end
-
-function ccr_allpolys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
-    polys = ccr_polys(pts)
-    integratepolys = polyint.(polys)
-    differentiatepolys = polyder.(polys)"""
-    catmullrom(points, interpolants)
-
-    `points` is a tuple of points-as-tuples
-    `interpolants` is a tuple of values from 0.0 to 1.0 (inclusive)
-
-interpolating points from points[2] through points[end-1] (inclusive)
-"""
-function catmullrom(points::NTuple{I, NTuple{D,T}}, interpolants::Union{A,NTuple{N,F}}) where {A<:AbstractArray, N, F, I, D, T}
-    npoints = length(points)
-    npoints < 4 && throw(ErrorException("at least four points are required"))
-
-    return points === 4 ? catmullrom_4points(points, interpolants) : catmullrom_npoints(points, interpolants)
-end
- 
-catmullrom(points::AbstractArray{F, N}, interpolants::NTuple{D,T}) where {F, N, D, T} =
-    catmullrom((points...,), interpolants)
-
-catmullrom(points::NTuple{I, NTuple{N,T}}, interpolants::AbstractArray{F, 1}) where {I, F, N, T} =
-    catmullrom(points, (interpolants...,))
-
-catmullrom(points::AbstractArray{T, N}, interpolants::AbstractArray{F, 1}) where {F, N, T} =
-    catmullrom((points...,), (interpolants...,))
-
-
-
-# ref https://ideone.com/NoEbVM
-
-# compute Catmull-Rom cubic curve over [0, 1]
-function catmullrom_cubic(x0::T, x1::T, x2::T, x3::T, dt0::T, dt1::T, dt2::T) where {T}
-    # compute tangents when parameterized in [t1,t2]
-    t1 = (x1 - x0) / dt0 - (x2 - x0) / (dt0 + dt1) + (x2 - x1) / dt1
-    t2 = (x2 - x1) / dt1 - (x3 - x1) / (dt1 + dt2) + (x3 - x2) / dt2
- 
-    # rescale tangents for parametrization in [0,1]
-    t1 *= dt1
-    t2 *= dt1
- 
-    # return hermite cubic over [0,1]
-    return hermite_cubic(x1, x2, t1, t2)
-end
-
-#=
-   Compute coefficients for a cubic polynomial
-     p(s) = c0 + c1*s + c2*s^2 + c3*s^3
-   such that
-     p(0) = x0, p(1) = x1
-    and
-     p'(0) = dx0, p'(1) = dx1.
-=#
-function hermite_cubic(x0::T, x1::T, dx0::T, dx1::T) where {T}
-    c0 = x0
-    c1 = dx0
-    c2 = -3*x0 + 3*x1 - 2*dx0 - dx1
-    c3 =  2*x0 - 2*x1 +   dx0 + dx1
-    return Poly([c0, c1, c2, c3])
-end
-
-
-#=
-   given four x-ordinate sequenced ND points
-   obtain N polys, parameterized over [0,1]
-   interpolating from p1 to p2 inclusive
-   one poly for each coordinate axis
-=#
-function ccr_polys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
-    dt0, dt1, dt2 = prep_centripetal_catmullrom(pts)
-    pt0, pt1, pt2, pt3 = pts
-    
-    polys = Vector{Poly{eltype(pts[1])}}(undef, N)
-
-    for i=1:N
-        polys[i] = catmullrom_cubic(pt0[i], pt1[i], pt2[i], pt3[i], dt0, dt1, dt2)
-    end
-
-    return polys
-end
-
-function ccr_polys_dpolys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
-    polys = ccr_polys(pts)
-    differentiatepolys = polyder.(polys)
-    return polys, differentiatepolys
-end
-
-function ccr_polys_ipolys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
-    polys = ccr_polys(pts)
-    integratepolys = polyint.(polys)
-    return polys, integratepolys
-end
-
-function ccr_allpolys(pts::NTuple{4, NTuple{N,T}}) where {N, T}
-    polys = ccr_polys(pts)
-    integratepolys = polyint.(polys)
-    differentiatepolys = polyder.(polys)
-    return polys, differentiatepolys, integratepolys
-end
 
 
 qrtrroot(x) = sqrt(sqrt(x))
@@ -360,3 +250,23 @@ end
 
     
     
+
+function catmullrom_polys_d01(pts::NTuple{4, NTuple{N,T}}) where {N, T}
+    polys = catmullrom_polys(pts)
+    d1polys = polyder.(polys)
+    return polys, d1polys
+end
+
+function catmullrom_polys_d012(pts::NTuple{4, NTuple{N,T}}) where {N, T}
+    polys = catmullrom_polys(pts)
+    d1polys = polyder.(polys)
+    d2polys = polyder.(d1polys)
+    return polys, d1polys, d2polys
+end
+
+function catmullrom_polys_i01(pts::NTuple{4, NTuple{N,T}}) where {N, T}
+    polys = catmullrom_polys(pts)
+    i1polys = polyint.(polys)
+    return polys, i1polys
+end
+
