2f732141a8d182c04453c6817497ad715e8e5fb9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-22 16:57:02 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -20,12 +20,21 @@ import LinearAlgebra: dot, norm
 # D is the dimensionality of the coordinate space
 # R is the eltype of points, the numeric type underlying each coordinate
 # M is the number of points, when using a tuple of points
-const TupledPoint{D,R} = NTuple{D,R} where {D,R}  
-const TupledPoints{M,D,R} = NTuple{M, TupledPoint{D,R}} where {M,D,R}
 # F is the numeric type used for values (usually should match R above)
 # L is the number of values, when using a tuple
-const ValueSeq{L,T} = Union{AbstractArray{T,1}, NTuple{L,T}} where {L,T}
-const PointSeq{M,D,T} = Union{AbstractArray{T,1}, TupledPoints{M,D,T}} where {M,D,T}
+
+const TupAsPoint{D,R} = NTuple{D,R} where {D,R}
+const VecAsPoint{R}   = AbstractArray{R,1} where {R}
+
+const TupAsPoints{M,D,R} = NTuple{M, TupAsPoint{D,R}} where {M,D,R}
+const VecAsPoints{R} = AbstractArray{VecAsPoint, 1} where {R}
+
+const TupAsValues{L,F} = NTuple{L,F} where {L,F}
+const VecAsValues{F} = AbstractArray{F,1|
+
+const PointSeq{M,D,R} = Union{TupAsPoints, VecAsPoints} where {M,D,R}
+const ValueSeq{L,T}   = Union{TupAsValues, VecAsValues} where {L,T}
+
 
 include("catmullrom.jl")
 include("interpolant.jl")
