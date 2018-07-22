ab7c2f8f743ff16337c499810612578057eef337
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-22 18:47:27 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -24,18 +24,22 @@ import LinearAlgebra: dot, norm
 # F is the numeric type used for values (usually should match R above)
 # L is the number of values, when using a tuple
 
-const TupAsPoint{D,R} = NTuple{D,R} where {D,R}
-const VecAsPoint{R}   = AbstractArray{R,1} where {R}
 
-const TupAsPoints{M,D,R} = NTuple{M, TupAsPoint{D,R}} where {M,D,R}
-const VecAsPoints{R} = AbstractArray{VecAsPoint{R}, 1} where {R}
+const TupAsPoint{D,R} = NTuple{D,R} where {D,R}
+const VecAsPoint{R}   = Vector{R}   where {R}
+const OnePoint{D,R}   = Union{TupAsPoint{D,R}, VecAsPoint{R}}  where {D,R}
+
+const TupAsTupPoints{M,D,R} = NTuple{M, TupAsPoint{D,R}} where {M,D,R}
+const TupAsVecPoints{M,R}   = NTuple{M, VecAsPoint{R}}   where {M,R}
+const VecAsTupPoints{D,R}   = Vector{TupAsPoint{D,R}}    where {D,R}
+const VecAsVecPoints{R}     = Vector{VecAsPoint{R}}      where {R}
+
+const PointSeq{M,D,R}  = Union{TupAsTupPoints{M,D,R}, TupAsVecPoints{M,R}, 
+                               VecAsTupPoints{D,R}, VecAsVecPoints{R}} where {M,D,R}
 
 const TupAsValues{L,F} = NTuple{L,F} where {L,F}
-const VecAsValues{F} = AbstractArray{F,1}
-
-const OnePoint{D,R}   = Union{TupAsPoint{D,R},  VecAsPoint{R}}  where {D,R}
-const PointSeq{M,D,R} = Union{TupAsPoints{M,D,R}, VecAsPoints{R}} where {M,D,R}
-const ValueSeq{L,F}   = Union{TupAsValues{L,F}, VecAsValues{F}} where {L,F}
+const VecAsValues{F}   = Vector{F} where {F}
+const ValueSeq{L,F}    = Union{TupAsValues{L,F}, VecAsValues{F}} where {L,F}
 
 
 include("catmullrom.jl")
