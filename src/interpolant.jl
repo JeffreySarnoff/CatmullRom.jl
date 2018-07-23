28ff428015956bdf072d172a61813ba5a9cf1217
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-22 20:21:33 -0400

    Update interpolant.jl

--- src/interpolant.jl
+++ src/interpolant.jl
@@ -27,75 +27,76 @@ chebyshevsep(n::Int) = n >= 2 ? cheb1zerosᵤ(n) : throw(DomainError("$n < 2"))
 cheb1zero(n,k) = cospi((n-k+0.5)/n)
 cheb1zeros(n) = [cheb1zero(n,k) for k=1:n]
 
-# shifted
-cheb1zeroₛ(n,k) = (cospi((n-k+0.5)/n) + 1)/2
-cheb1zerosₛ(n) = [cheb1zeroₛ(n,k) for k=1:n]
+# shifted into 0..1
+cheb1zero_shifted(n,k) = (cospi((n-k+0.5)/n) + 1)/2
+cheb1zeros_shifted(n) = [cheb1zero_shifted(n,k) for k=1:n]
 
-# bounded by 0,1
-cheb1zerosᵤ(n) = [0.0, [cheb1zeroₛ(n-2,k) for k=1:n-2]..., 1.0]
+# includes 0,1 as first, last
+shifted_cheb1zeros(n) = [0.0, [cheb1zero_shifted(n-2,k) for k=1:n-2]..., 1.0]
 
 # zeros of U(x)
 
 cheb2zero(n,k) = cospi((n-k+1)/(n+1))
 cheb2zeros(n) = [cheb2zero(n,k) for k=1:n]
 
-# shifted
+# shifted into 0..1
 cheb2zeroₛ(n,k) = (cospi((n-k+1)/(n+1)) + 1)/2
 cheb2zerosₛ(n) = [cheb2zeroₛ(n,k) for k=1:n]
 
-# bounded by 0,1
-cheb2zerosᵤ(n) = [0.0, [cheb2zeroₛ(n-2,k) for k=1:n-2]..., 1.0]
+# includes 0,1 as first, last
+shifted_cheb2zeros(n) = [0.0, [cheb2zeroₛ(n-2,k) for k=1:n-2]..., 1.0]
 
 # zeros of V(x)
 
 cheb3zero(n,k) = cospi((n-k+0.5)/(n+0.5))
 cheb3zeros(n) = [cheb3zero(n,k) for k=1:n]
 
-# shifted
-cheb3zeroₛ(n,k) = (cospi((n-k+0.5)/(n+0.5)) + 1)/2
-cheb3zerosₛ(n) = [cheb3zeroₛ(n,k) for k=1:n]
+# shifted into 0..1
+cheb3zero_shifted(n,k) = (cospi((n-k+0.5)/(n+0.5)) + 1)/2
+cheb3zeros_shifted(n) = [cheb3zero_shifted(n,k) for k=1:n]
 
-# bounded by 0,1
-cheb3zerosᵤ(n) = [0.0, [cheb3zeroₛ(n-2,k) for k=1:n-2]..., 1.0]
+# includes 0,1 as first, last
+shifted_cheb3zeros(n) = [0.0, [cheb3zero_shifted(n-2,k) for k=1:n-2]..., 1.0]
 
 # zeros of W(x)
 
 cheb4zero(n,k) = cospi((n-k+1)/(n+0.5))
 cheb4zeros(n) = [cheb4zero(n,k) for k=1:n]
 
-# shifted
-cheb4zeroₛ(n,k) = (cospi((n-k+1)/(n+0.5)) + 1)/2
-cheb4zerosₛ(n) = [cheb4zeroₛ(n,k) for k=1:n]
+# shifted into 0..1
+cheb4zero_shifted(n,k) = (cospi((n-k+1)/(n+0.5)) + 1)/2
+cheb4zeros_shifted(n) = [cheb4zero_shifted(n,k) for k=1:n]
 
-# bounded by 0,1
-cheb4zerosᵤ(n) = [0.0, [cheb4zeroₛ(n-2,k) for k=1:n-2]..., 1.0]
+# includes 0,1 as first, last
+shifted_cheb4zeros(n) = [0.0, [cheb4zero_shifted(n-2,k) for k=1:n-2]..., 1.0]
 
 
 # extrema of _weighted_ Chebyshev polynomials (types 1,2,3,4)
+# includes reverse order 1-extrema with extrema 
 
 # extrema of T(x)
 
 cheb1extrema(n,k) = cospi((n-k)/n)
 cheb1extrema(n) = [cheb1extrema(n-1,k) for k=0:n-1]
 
-# shifted
-cheb1extremaₛ(n,k) = (cospi((n-k)/n) + 1)/2
-cheb1extremaₛ(n) = [cheb1extremaₛ(n-1,k) for k=0:n-1]
+# shifted into 0,1
+cheb1extrema_shifted(n,k) = (cospi((n-k)/n) + 1)/2
+cheb1extrema_shifted(n) = [cheb1extrema_shifted(n-1,k) for k=0:n-1]
 
-# bounded by 0,1
-cheb1extremaᵤ(n) = [cheb1extremaₛ(n-1,k) for k=0:n-1]
+# includes 0,1 as first, last
+shifted_cheb1extrema(n) = [cheb1extrema_shifted(n-1,k) for k=0:n-1]
 
 # extrema of sqrt(1-x^2) * U(x)
 
 cheb2extrema(n,k) = cospi((2*(n-k)+1)/(2*(n+1)))
 cheb2extrema(n) = [cheb2extrema(n-1,k) for k=0:n-1]
 
-# shifted
-cheb2extremaₛ(n,k) = (cospi((2*(n-k)+1)/(2*(n+1)))+1)/2
-cheb2extremaₛ(n) = [cheb2extremaₛ(n-1,k) for k=0:n-1]
+# shifted into 0,1
+cheb2extrema_shifted(n,k) = (cospi((2*(n-k)+1)/(2*(n+1)))+1)/2
+cheb2extrema_shifted(n) = [cheb2extrema_shifted(n-1,k) for k=0:n-1]
 
-# bounded by 0,1
-cheb2extremaᵤ(n) = [0.0, [cheb2extremaₛ(n-3,k) for k=0:n-3]..., 1.0]
+# includes 0,1 as first, last
+shifted_cheb2extremaᵤ(n) = [0.0, [cheb2extrema_shifted(n-3,k) for k=0:n-3]..., 1.0]
 
 
 # extrema of sqrt(1+x) * V(x)
@@ -103,21 +104,21 @@ cheb2extremaᵤ(n) = [0.0, [cheb2extremaₛ(n-3,k) for k=0:n-3]..., 1.0]
 cheb3extrema(n,k) = cospi((2*(n-k))/(2*n+1))
 cheb3extrema(n) = [cheb3extrema(n,k) for k=0:n-1]
 
-# shifted
-cheb3extremaₛ(n,k) = (cospi((2*(n-k))/(2*n+1)) + 1)/2
-cheb3extremaₛ(n) = [cheb3extremaₛ(n,k) for k=0:n-1]
+# shifted into 0,1
+cheb3extrema_shifted(n,k) = (cospi((2*(n-k))/(2*n+1)) + 1)/2
+cheb3extrema_shifted(n) = [cheb3extrema_shifted(n,k) for k=0:n-1]
 
-# bounded by 0,1
-cheb3extremaᵤ(n) = [0.0, [cheb3extremaₛ(n-2,k) for k=0:n-3]..., 1.0]
+# includes 0,1 as first, last
+shifted_cheb3extrema(n) = [0.0, [cheb3extrema_shifted(n-2,k) for k=0:n-3]..., 1.0]
 
 # extrema of sqrt(1-x) * W(x)
 
 cheb4extrema(n,k) = cospi((2*(n-k)+1)/(2*n+1))
 cheb4extrema(n) = [cheb4extrema(n,k) for k=1:n]
 
-# shifted
-cheb4extremaₛ(n,k) = (cospi((2*(n-k)+1)/(2*n+1)) + 1)/2
-cheb4extremaₛ(n) = [cheb4extremaₛ(n,k) for k=1:n]
+# shifted into 0,1
+cheb4extrema_shifted(n,k) = (cospi((2*(n-k)+1)/(2*n+1)) + 1)/2
+cheb4extrema_shifted(n) = [cheb4extrema_shifted(n,k) for k=1:n]
 
-# bounded by 0,1
-cheb4extremaᵤ(n) = [0.0, [cheb4extremaₛ(n-2,k) for k=1:n-2]..., 1.0]
+# includes 0,1 as first, last
+shifted_cheb4extrema(n) = [0.0, [cheb4extrema_shifted(n-2,k) for k=1:n-2]..., 1.0]
