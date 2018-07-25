30ec8516c3da887e0e09915ad34cb9450c8fac7f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-24 21:06:50 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -17,31 +17,6 @@ import Polynomials: Poly, polyval, polyder, polyint
 
 import LinearAlgebra: dot, norm
 
-
-# D is the dimensionality of the coordinate space
-# R is the eltype of points, the numeric type underlying each coordinate
-# M is the number of points, when using a tuple of points
-# F is the numeric type used for values (usually should match R above)
-# L is the number of values, when using a tuple
-
-
-const TupAsPoint{D,R} = NTuple{D,R} where {D,R}
-const VecAsPoint{R}   = Vector{R}   where {R}
-const OnePoint{D,R}   = Union{TupAsPoint{D,R}, VecAsPoint{R}}  where {D,R}
-
-const TupAsTupPoints{M,D,R} = NTuple{M, TupAsPoint{D,R}} where {M,D,R}
-const TupAsVecPoints{M,R}   = NTuple{M, VecAsPoint{R}}   where {M,R}
-const VecAsTupPoints{D,R}   = Vector{TupAsPoint{D,R}}    where {D,R}
-const VecAsVecPoints{R}     = Vector{VecAsPoint{R}}      where {R}
-
-const PointSeq{M,D,R}  = Union{TupAsTupPoints{M,D,R}, TupAsVecPoints{M,R}, 
-                               VecAsTupPoints{D,R}, VecAsVecPoints{R}} where {M,D,R}
-
-const TupAsValues{L,F} = NTuple{L,F} where {L,F}
-const VecAsValues{F}   = Vector{F} where {F}
-const ValueSeq{L,F}    = Union{TupAsValues{L,F}, VecAsValues{F}} where {L,F}
-
-
 include("catmullrom.jl")
 include("interpolant.jl")
 include("arcbased.jl")
