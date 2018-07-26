3013b3caa762a0a6270ad823b4ae3ed17a971ed3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-26 16:01:42 -0400

    Create unions.jl

---
+++ src/unions.jl
@@ -0,0 +1,14 @@
+const TupOfValues{L,F} = NTuple{L,F} where {L,F}
+const VecOfValues{F}   = Vector{F} where {F}
+const ValueSeq{L,F}    = Union{TupAsValues{L,F}, VecAsValues{F}} where {L,F}
+
+const TupAsPoint{D,R} = NTuple{D,R} where {D,R}
+const VecAsPoint{R}   = Vector{R}   where {R}
+
+const TupOfTupPoints{M,D,R} = NTuple{M, TupAsPoint{D,R}} where {M,D,R}
+const TupOfVecPoints{M,R}   = NTuple{M, VecAsPoint{R}}   where {M,R}
+const VecOfTupPoints{D,R}   = Vector{TupAsPoint{D,R}}    where {D,R}
+const VecOfVecPoints{R}     = Vector{VecAsPoint{R}}      where {R}
+
+const PointSeq{M,D,R}  = Union{TupOfTupPoints{M,D,R}, TupOfVecPoints{M,R}, 
+                               VecOfTupPoints{D,R}, VecOfVecPoints{R}} where {M,D,R}
