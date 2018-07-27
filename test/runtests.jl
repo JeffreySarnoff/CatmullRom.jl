5e4c1ebeeef3d36fa96083b75ded5f93774a8a6e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 00:21:46 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using CentripetalCatmullRom
+using CatmullRom
 using Test
 
 points2D = ((0.0, 0.0), (0.25, 2.0), (0.75, 2.0), (1.0, 0.0))
@@ -6,8 +6,8 @@ points3D = ((0.0, 0.0, 0.0), (0.25, 2.0, 0.5), (1.75, 2.0, 1.0), (1.0, 0.0, 0.0)
 
 interpolants = collect(0.0:0.2:1.0)
 
-pts2D = catmullrom(points2D, interpolants)
-pts3D = catmullrom(points3D, interpolants)
+pts2D = catmullrom(points2D, interpolants, endpoints=Omit)
+pts3D = catmullrom(points3D, interpolants, endpoints=Omit)
 
 @test Float32.(pts2D[1,:]) == Float32.([0.25, 2.0])
 @test Float32.(pts3D[1,:]) == Float32.([0.25, 2.0, 0.5])
@@ -15,5 +15,5 @@ pts3D = catmullrom(points3D, interpolants)
 
 points2D = ((0.0, 0.0), (0.25, 2.0), (0.5, 3.0), (0.75, 2.0), (1.0, 0.0))
 interpolants = collect(0.0:0.2:1.0)
-pts2D = catmullrom(points2D, interpolants)
+pts2D = catmullrom(points2D, interpolants, endpoints=Omit)
 @test pts2D[6,:] == [0.5, 3.0]
