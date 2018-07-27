169f9916c61df18cfcfe6e29b2559fa41b9ceecc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 01:31:16 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -3,7 +3,7 @@ __precompile__()
 module CatmullRom
 
 export catmullrom,    # points, interpolants --> points, interpolated points
-       Thiele3, Omit,
+       Quadratic, Thiele3, Omit,
        uniformspacing, into01,
        Poly, polyval, polyder, polyint  # reexported
 
@@ -12,8 +12,9 @@ import Polynomials: Poly, polyval, polyder, polyint
 
 import LinearAlgebra: dot, norm
 
-const Thiele3 = :Thiele3
-const Omit    = :Omit
+const Quadratic = :Quadratic
+const Thiele3   = :Thiele3
+const Omit      = :Omit
 
 include("unions.jl")
 include("catmullrom.jl")
