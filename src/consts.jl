3d3673caf32d02f0b48abb4402002e934b1b4da2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 02:27:00 -0400

    Update consts.jl

--- src/consts.jl
+++ src/consts.jl
@@ -2,5 +2,3 @@ const Linear    = :Linear
 const Quadratic = :Quadratic
 const Thiele3   = :Thiele3
 const Omit      = :Omit
-
-const endpointfn = Dict([:Linear => linear, :Quadratic => quadratic, :Thiele3 => thiele3])
