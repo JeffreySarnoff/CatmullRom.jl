af79847ac1c8a3c9b40c6998e7c6c114e770e3cd
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 01:39:44 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -3,7 +3,7 @@ __precompile__()
 module CatmullRom
 
 export catmullrom,    # points, interpolants --> points, interpolated points
-       Quadratic, Thiele3, Omit,
+       Omit, Linear, Quadratic, Thiele3
        uniformspacing, into01,
        Poly, polyval, polyder, polyint  # reexported
 
