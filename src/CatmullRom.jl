751c7d3f8b776dbdf97d88844a0823254afbacf8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 02:23:33 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -12,6 +12,7 @@ import Polynomials: Poly, polyval, polyder, polyint
 
 import LinearAlgebra: dot, norm
 
+const Linear    = :Linear
 const Quadratic = :Quadratic
 const Thiele3   = :Thiele3
 const Omit      = :Omit
