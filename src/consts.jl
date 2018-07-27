1ff62523d2b2d5a255c14a2c5f816a396f6c9107
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-27 02:29:27 -0400

    Update consts.jl

--- src/consts.jl
+++ src/consts.jl
@@ -2,3 +2,10 @@ const Linear    = :Linear
 const Quadratic = :Quadratic
 const Thiele3   = :Thiele3
 const Omit      = :Omit
+
+# stubs
+linear()    = ()
+quadratic() = ()
+thiele3()   = ()
+
+const endpointfn = Dict([:Linear => linear, :Quadratic => quadratic, :Thiele3 => thiele3])
