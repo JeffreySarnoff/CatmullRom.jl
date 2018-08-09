2039657046d0cf6e12877efe17c8ea22daef3d86
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-10 00:46:59 +0100

    Update interpolant.jl

--- src/interpolant.jl
+++ src/interpolant.jl
@@ -1,22 +1,49 @@
 """
-    uniformspacing(n::Int)
+    clamp01(x)
 
-generate n values uniformly spaced between 0..1 (n does not count including of 0,1) 
+min(max(0, x), 1)
 """
-uniformspacing(n::Int) = n >= 0 ? collect(0.0:inv((n+2)-1):1.0) : throw(DomainError("$n < 0"))
-
 @inline clamp01(x::T) where {T<:Real} = clamp(x, zero(T), one(T))
+
+"""
+    within01(n::Int)
+
+generate n >= 0 values uniformly spaced inside 0..1, excludes 0, 1
+
+# Examples
+    within01(0) == []
+    within01(1) == [1/2]
+    within01(2) == [1/3, 2/3]
+"""
+within01(n::Int) = collect(0.0:inv(n-1):1.0)
+
+"""
+    uniform01(n::Int)
+
+generate n >= 2 values uniformly spaced from 0..1, includes 0, 1
+
+# Examples
+    uniform01(2) == [0.0, 1.0]
+    uniform01(3) == [0.0, 0.5, 1.0]
+    uniform01(4) == [0.0, 1/3, 2/3, 1.0]
+"""
+uniform01(n::Int) = LinRange(0.0, 1.0, max(2,n))
 
 """
     into01((xs...,))
 
 maps values into 0.0:1.0 (minimum(xs) --> 0.0, maximum(xs) --> 1.0)
 """
-function into01(values::ValueSeq) where {F,L}
-    mn, mx = minimum(values), maximum(values)
-    delta = mx .- mn
-    delta = sqrt(sum(map(x->x*x, delta)))
-    result = collect(clamp01((v .- mn)./delta) for v in values)
+function into01(unitrange::UnitRange{T}) where {T}
+    span = unitrange.stop - unitrange.start
+    return LinRange(0.0, 1.0, span)
+end
+
+function into01(values::U) where {U<:Union{AbstractVector{T}, NTuple{N,T}) where {N,T}
+    valuemin = minimum(values)
+    span = maximum(values) - valuemin
+    delta = sqrt(sum(map(x->x*x, span)))
+    result = collect(clamp01((v .- valuemin)./span) for v in values)
     return result
 end
 
