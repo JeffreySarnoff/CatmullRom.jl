2abbf789cd3a8b99c1cb78fdf0c6add1efb76cd0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-10 04:28:54 +0100

    Update unions.jl

--- src/unions.jl
+++ src/unions.jl
@@ -1,16 +1,3 @@
-const TupOfValues{L,F} = NTuple{L,F} where {L,F}
-const VecOfValues{F}   = Vector{F} where {F}
-const ValueSeq{L,F}    = Union{TupOfValues{L,F}, VecOfValues{F}} where {L,F}
-
-const TupAsPoint{D,R} = NTuple{D,R} where {D,R}
-const VecAsPoint{R}   = Vector{R}   where {R}
-const OnePoint{D,R}   = Union{TupAsPoint, VecAsPoint} where {D,R}
-
-const TupOfTupPoints{M,D,R} = NTuple{M, TupAsPoint{D,R}} where {M,D,R}
-const TupOfVecPoints{M,R}   = NTuple{M, VecAsPoint{R}}   where {M,R}
-const VecOfTupPoints{D,R}   = Vector{TupAsPoint{D,R}}    where {D,R}
-const VecOfVecPoints{R}     = Vector{VecAsPoint{R}}      where {R}
-
-const PointSeq{M,D,R}  =
-    Union{TupOfTupPoints{M,D,R}, TupOfVecPoints{M,R},
-          VecOfTupPoints{D,R}, VecOfVecPoints{R}} where {M,D,R}
+const OnePoint{N,T} = Union{Vector{T}, NTuple{N,T}} where {N,T}
+const ValueSeq{N,T} = Union{Vector{T}, NTuple{N,T}} where {N,T}
+const PointSeq{N,T} = Union{Vector{T}, NTuple{N,T}} where {N,T}
