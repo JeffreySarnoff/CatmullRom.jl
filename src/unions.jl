19f4905e3d6c7e466182badff0783feaa0c135ce
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-18 03:34:13 -0400

    Update unions.jl

--- src/unions.jl
+++ src/unions.jl
@@ -1,3 +1,3 @@
-const OnePoint{N,T} = Union{Vector{T}, NTuple{N,T}} where {N,T}
-const ValueSeq{N,T} = Union{Vector{T}, NTuple{N,T}} where {N,T}
-const PointSeq{N,T} = Union{Vector{T}, NTuple{N,T}} where {N,T}
+const OnePoint{N1,T1} = Union{Vector{T1}, NTuple{N1,T1}} where {N1,T1}
+const ValueSeq{N2,T2} = Union{Vector{T3}, NTuple{N2,T2}} where {N2,T2}
+const PointSeq{N3,T3} = Union{Vector{T3}, NTuple{N3,T3}} where {N3,T2}
