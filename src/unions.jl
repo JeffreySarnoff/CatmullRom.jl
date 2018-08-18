445aedac35386244ac91723b339d299524f6aeea
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-18 08:42:53 -0400

    Update unions.jl

--- src/unions.jl
+++ src/unions.jl
@@ -1,2 +1,2 @@
 const OnePoint{N,T} = NTuple{N,T} where {N,T}
-const Items{N,T} = Union{A, NT} where {N,T, A<:Vector{T}, NT<:NTuple{N,T}}
+const Items{N,T} = Union{A, NT} where {N,T, A<:Vector{T}, NT<:NTuple{N,Tuple{Vararg{T}}}}
