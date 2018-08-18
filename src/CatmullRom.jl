00a0173b3f979ab27a0d27bb2fdbcc5bf4be6ea1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-18 07:08:14 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -1,3 +1,12 @@
+"""
+   CatmullRom
+
+This package provides open and closed centripetal Catmull-Rom splines.
+The centripetal parameterization of Catmull-Rom splines works best in use:
+it does not crimp and provides a useful amount of relative taughtness.
+
+see @ref(catmullrom) for use.
+"""
 module CatmullRom
 
 export catmullrom,    # points, interpolants --> points, interpolated points
