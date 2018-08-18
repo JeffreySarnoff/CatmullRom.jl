c5731f56d12c28bb5f0e9f651f831072013c84d0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-18 06:07:38 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ include("interpolants.jl")
 points2D = ((0.0, 0.0), (0.25, 2.0), (0.75, 2.0), (1.0, 0.0))
 points3D = ((0.0, 0.0, 0.0), (0.25, 2.0, 0.5), (1.75, 2.0, 1.0), (1.0, 0.0, 0.0))
 
-interpolants = collect(0.0:0.2:1.0)
+interpolants = (collect(0.0:0.2:1.0)...,)
 
 pts2D = catmullrom(points2D, interpolants, endpoints=Omit)
 pts3D = catmullrom(points3D, interpolants, endpoints=Omit)
