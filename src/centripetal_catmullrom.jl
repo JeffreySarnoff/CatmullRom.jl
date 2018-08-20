36eb33a239551934ae4b853a27d0781769426d19
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-20 00:50:44 -0400

    Update centripetal_catmullrom.jl

--- src/centripetal_catmullrom.jl
+++ src/centripetal_catmullrom.jl
@@ -1,7 +1,7 @@
 """
      catmullrom(points, ninterpolants::Int; endpoints::Symbol=Thiele3, closed::Bool=false)
      catmullrom(points, interpolants; endpoints::Symbol=Thiele3, closed::Bool=false)
-   
+
     `points` is a tuple/vector of points-as-tuples/vectors
     `interpolants` is a tuple/vector of values from 0.0 to 1.0 (inclusive)
 
@@ -10,103 +10,46 @@ all of the interpolants are applied to each segment
 endpoints==Omit: interpolating from points[2] through points[end-1]
 endpoints==Thiele3:  interpolating from points[1] through points[end]
 """
-function catmullrom(points::Items{N,T}, interpolants::Items{M,S}; endpoints::Symbol=Thiele3, closed::Bool=false) where {N,T,M,S}
+function catmullrom( points::Points{N,T},
+                     interpolants::Seq01{M,S};
+                     endpoints::Symbol=Thiele3,
+                     closed::Bool=false
+                   ) where {N,M,T<:Real,S<:AbstractFloat}
+
     npoints = length(points)
     npoints < 4 && throw(ErrorException("at least four points are required"))
 
-    return if endpoints === Omit
-               npoints > 4 ? catmullrom_npoints(points, interpolants) : catmullrom_4points(points, interpolants)
-           else
-               TY = typeof(points[1]) <: Tuple ? Tuple : Vector
-               points = augmentends(TY, endpointfn[endpoints], points, closed)
-               catmullrom_npoints(points, interpolants)
-           end
+    if endpoints != Omit
+        TY = typeof(points[1]) <: Tuple ? Tuple : Vector
+        points = augmentends(TY, endpointfn[endpoints], points, closed)
+        npoints = length(points)
+    end
+    if npoints === 4
+        catmullrom_4points(points, interpolants)
+    else
+        catmullrom_npoints(points, interpolants)
+    end
 end
 
-function catmullrom(points::Items{N,T}, ninterpolants::Integer; endpoints::Symbol=Thiele3, closed::Bool=false) where {N,T}
+function catmullrom_augmented( points::Points{N,T},
+                               interpolants::Seq01{M,S};
+                               endpoints::Symbol,
+                               closed::Bool
+                              ) where {N,M,T<:Real,S<:AbstractFloat}
+
+     TY = typeof(points[1]) <: Tuple ? Tuple : Vector
+     points = augmentends(TY, endpointfn[endpoints], points, closed)
+     return catmullrom_npoints(points, interpolants)
+end
+
+function catmullrom(points::Points{N,T}, ninterpolants::Integer; endpoints::Symbol=Thiele3, closed::Bool=false) where {N,T}
     interpolants = uniform01(ninterpolants)
     return catmullrom(points, interpolants, endpoints=endpoints, closed=closed)
-end
-
-@inline function augmentends(::Type{Tuple}, endpoints::Function, points::Items{N,T}, closed) where {N,T}
-    if !closed
-        pre  = prepoint(endpoints, points[1:3]...,)
-        post = postpoint(endpoints, points[end-2:end]...,)
-    else
-        pre  = points[end-2:end]
-        post = points[1:3]
-        #=  
-        pre   = (points[end] .+ points[1] .+ points[1])./3
-        post  = (points[end] .+ points[end] .+ points[1])./3
-        =#
-    end
-    
-    return ((pre...,), points[1:end]..., (post...,))
-end
-
-@inline function augmentends(::Type{Vector}, endpoints::Symbol, points::Items{N,T}, closed) where {N,T}
-    if !closed
-        pre  = prepoint(endpoints, points[1:3]...,)
-        post = postpoint(endpoints, points[end-2:end]...,)
-    else
-        pre  = points[end-2:end]
-        post = points[1:3]
-    end
-    
-    return ((pre...,), points[1:end]..., (post...,))
 end
 
 
 # ref https://ideone.com/NoEbVM
 
-# compute Catmull-Rom cubic curve over [0, 1]
-function catmullrom_cubic(x0::T, x1::T, x2::T, x3::T, dt0::T, dt1::T, dt2::T) where {T}
-    # compute tangents when parameterized in [t1,t2]
-    t1 = (x1 - x0) / dt0 - (x2 - x0) / (dt0 + dt1) + (x2 - x1) / dt1
-    t2 = (x2 - x1) / dt1 - (x3 - x1) / (dt1 + dt2) + (x3 - x2) / dt2
-
-    # rescale tangents for parametrization in [0,1]
-    t1 *= dt1
-    t2 *= dt1
-
-    # return hermite cubic over [0,1]
-    return hermite_cubic(x1, x2, t1, t2)
-end
-
-#=
-   Compute coefficients for a cubic polynomial
-     p(s) = c0 + c1*s + c2*s^2 + c3*s^3
-   such that
-     p(0) = x0, p(1) = x1
-    and
-     p'(0) = dx0, p'(1) = dx1.
-=#
-function hermite_cubic(x0::T, x1::T, dx0::T, dx1::T) where {T}
-    c0 = x0
-    c1 = dx0
-    c2 = -3*x0 + 3*x1 - 2*dx0 - dx1
-    c3 =  2*x0 - 2*x1 +   dx0 + dx1
-    return Poly([c0, c1, c2, c3])
-end
-
-qrtrroot(x) = sqrt(sqrt(x))
-
-#=
-   determine the delta_traversal constants for the centripetal parameterization
-      of the Catmull Rom cubic specified by four points (of increasing abcissae)
-=#
-function prep_centripetal_catmullrom(points::Items{N,T}) where {N,T}
-    dt0 = qrtrroot(dot(points[1], points[2]))
-    dt1 = qrtrroot(dot(points[2], points[3]))
-    dt2 = qrtrroot(dot(points[3], points[4]))
-
-    #safety check for repeated points
-    if (dt1 < 1e-6) dt1 = 1.0 end
-    if (dt0 < 1e-6) dt0 = dt1 end
-    if (dt2 < 1e-6) dt2 = dt1 end
-
-    return dt0, dt1, dt2
- end
 
 """
     catmullrom_npoints(points, interpolants)
@@ -114,7 +57,7 @@ function prep_centripetal_catmullrom(points::Items{N,T}) where {N,T}
     `interpolants` is a tuple of values from 0.0 to 1.0 (inclusive)
 interpolating points from points[2] through points[end-1] (inclusive)
 """
-function catmullrom_npoints(pts::Items{N,T}, interpolants::Items{M,S}) where {N,T,M,S}
+function catmullrom_npoints(pts::Points{N,T}, interpolants::Seq01{M,S}) where {N,T,M,S}
     npoints = length(pts)
     points_per_interpolation = length(interpolants)
     totalinterps = (npoints-4+1)*(points_per_interpolation - 1) + 1 # -1 for the shared end|1 point
@@ -142,6 +85,7 @@ function catmullrom_npoints(pts::Items{N,T}, interpolants::Items{M,S}) where {N,
     return points
 end
 
+
 #=
    given four ND points in increasing abcissae order
    and k+2 interpolant values in 0..1 where 0.0 and 1.0 are the +2
@@ -149,7 +93,7 @@ end
    where the first interpolant point is the second ND point
    and the final interplant point is the third ND point
 =#
-function catmullrom_4points(pts::Items{N,T}, interpolants::Items{M,S}) where {N,T,M,S}
+function catmullrom_4points(pts::Points{N,T}, interpolants::Seq01{M,S}) where {N,T,M,S}
     polys = catmullrom_polys(pts)
     totalinterps = length(interpolants)
     dimen = length(pts[1])
@@ -174,35 +118,3 @@ function catmullrom_4points(pts::Items{N,T}, interpolants::Items{M,S}) where {N,
     return [(points[i,:]...,) for i=1:size(points)[1]]
 end
 
-#=
-   given four x-ordinate sequenced ND points
-   obtain N polys, parameterized over [0,1]
-   interpolating from p1 to p2 inclusive
-   one poly for each coordinate axis
-=#
-function catmullrom_polys(points::Items{N,T}, interpolants::Items{M,S}) where {N,T,M,S}
-    length(points) == 4 || throw(DomainError("exactly four points are required"))
-
-    dt0, dt1, dt2 = prep_centripetal_catmullrom(points)
-    pt0, pt1, pt2, pt3 = points
-
-    dimen = length(pt0)
-    polys = Vector{Poly{eltype(points[1])}}(undef, dimen)
-
-    for i=1:dimen
-        polys[i] = catmullrom_cubic(pt0[i], pt1[i], pt2[i], pt3[i], dt0, dt1, dt2)
-    end
-
-    return polys
-end
-
-function catmullrom_allpolys(points::Items{N,T}, deriv1::Bool=false, deriv2::Bool=false, integ1::Bool=false) where {N,T}
-    polys = catmullrom_polys(points)
-
-    d1polys = deriv1 ? polyder.(polys)    : nothing
-    d2polys = deriv2 ? polyder.(d1polys)  : nothing
-    i1polys = integ1 ? polyint.(polys)    : nothing
-
-    result = (polys, d1polys, d2polys, i1polys)
-    return result
-end
