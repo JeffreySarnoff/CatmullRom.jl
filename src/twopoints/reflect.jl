46df6c100ad7d086ed1ee73e6480f2828f1f21f4
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-13 07:57:47 -0400

    Create reflect.jl

---
+++ src/twopoints/reflect.jl
@@ -0,0 +1,7 @@
+reflectback(pt₁::T, pt₂::T; scale=1.0) where {T}
+   @. pt₁ - ((pt₂ - pt₁) * scale)
+end
+
+function reflectforward(pt₁::T, pt₂::T; scale=1.0) where {T}
+    return @. pt₂ - ((pt₁ - pt₂) * scale)
+end
