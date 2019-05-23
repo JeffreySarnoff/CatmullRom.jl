5d62948c2d5dfb1080f20bc4295a3d5859207e3d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-23 14:07:13 -0400

    Create outside.jl

---
+++ src/fewpoints/outside.jl
@@ -0,0 +1,18 @@
+#=
+   Determine two extremal points (outside of the boundry points)
+   for use in anchoring the sequence of centripetal Catmull-Rom
+   spline spans to incorporate the boundry points in the spline.
+=#
+
+function outside(initialpoints::T, finalpoints::T) where {T}
+    ninitials = length(initialpoints)
+    nfinals   = length(finalpoints)
+    (ninitials > 1 && nfinals > 1) || throw(DomainError("missing points"))
+    
+    xinitial = pre()
+    xfinal   = post()
+    initial = ninitials > 2 ? thiele3(initialpoints[1:3], xinitial) : linear(initialpoints[1:2], xinitial)
+    final  = ninitials > 2 ? thiele3(finalpoints[1:3], xfinal) : linear(finalpoints[1:2], xfinal)
+
+    return initial, final
+end
