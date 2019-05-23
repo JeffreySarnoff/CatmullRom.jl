86695aed88f390b9935ca5f1b3b59992ee46d312
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-23 11:17:18 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -12,6 +12,8 @@ export catmullrom,
        reflect, linear, quadratic, thiele3, thiele4  # also for interpolation
 
 include("centripetal/catmullrom.jl")
+
+include("fewpoints/augmentends.jl")
 include("fewpoints/twopoints.jl")
 include("fewpoints/threepoints.jl")
 include("fewpoints/fourpoints.jl")
