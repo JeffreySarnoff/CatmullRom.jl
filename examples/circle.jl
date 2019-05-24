19647d955117e579465002f6d01a715b75cb427d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-24 07:47:56 -0400

    Update circle.jl

--- examples/circle.jl
+++ examples/circle.jl
@@ -1,3 +1,12 @@
-nbetween = 12
-ts = range(-1.0, 1.0, length=nbetween)
-xs = cos.(ts); ys = sin.(ts);
+"""
+
+Equations for interpolatory m-ordinates (vectorial coordinate systems have a coordinate value connection.
+
+x_circle(t) = cos(t)    y_circle(t) = sin(t)
+
+
+that walks pathsground sequency dimensional path coordinate  an  hapoints along path-coherent neighbors with 1 or 2 or 3+
+connected circular  
+
+Points that satisfy a given circle, that coincide with intent as a path, are parsimoniously gathered.
+
