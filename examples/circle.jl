972a7f2b63c16d2ee6e09e26cf87a8590a0b3e30
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-25 05:51:56 -0400

    Update circle.jl

--- examples/circle.jl
+++ examples/circle.jl
@@ -1,12 +1,32 @@
 """
+Approaching the Unit Circle
 
+    x(t) = cospi(t),    y(t) = sinpi(t)
+    eigvecs(x, y) covers a circle as t ↦ 0..2
+    
+We need 4 points to determine a Catmull-Rom spline over the _middle_ 2 points. 
+circleis a closed curve, so each pair of adjacent points is _middle_ to the other twts.
+
+given₁ = [ (x= 1.0, y= 0.0), (x= 0.0, y= 1.0), (x= -1.0, y=0 .0), (x= 0.0, y= -1.0) ]
+
+xs = first.(given₁)
+ys = final.(given₁)
+
+Is that too coarse a list? Are there too few points given to support our purpose?
+
+given₂ = [ (x=  1.0, y=  0.0), ( √3/2,  1/2), ( √2/2,  √2/2), ( 1/2,  √3/2), 
+           (x=  0.0, y=  1.0), ( -1/2, √3/2), (-√2/2,  √2/2), (-√3/2,  1/2), 
+           (x= -1.0, y=  0.0), (-√3/2, -1/2), (-√2/2, -√2/2), (-1/2, -√3/2),
+           (x=  0.0, y= -1.0), ( √3/2,  1/2), ( √2/2,  √2/2), ( 1/2,  √3/2) ]
+
+xs, ys = given₁
+xs, ys = given₁
+
+
+How many intermediating points should be introduced between adjacent point pairs?
+
+
+the between  will that  
+x( 0..2π / = The `x` and `y` coordinates are Along the circumference Fitting 
 Equations for interpolatory m-ordinates (vectorial coordinate systems have a coordinate value connection.
-
-x_circle(t) = cos(t)    y_circle(t) = sin(t)
-
-
-that walks pathsground sequency dimensional path coordinate  an  hapoints along path-coherent neighbors with 1 or 2 or 3+
-connected circular  
-
-Points that satisfy a given circle, that coincide with intent as a path, are parsimoniously gathered.
 
