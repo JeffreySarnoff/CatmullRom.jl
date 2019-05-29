a9a1d98820d085d780f127bb1a0681530c3957e3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-29 08:01:36 -0400

    Create support.jl

---
+++ src/centripetal/support.jl
@@ -0,0 +1,14 @@
+isclosed(firstpoint::V, lastpoint::V) where {T, V<:AbstractVector{T}} = 
+    sum(abs.(lastpoint .- firstpoint)) < eps(T)^(5/8)
+
+isclosed(firstpoint::NTuple{N,T}, lastpoint::NTuple{N,T}) where {N,T} = 
+    sum(abs.(lastpoint .- firstpoint)) < eps(T)^(5/8)
+    
+isclosed(points) = isclosed(points[1], points[end])
+
+augment(points) =
+    isclosed(points[1], points[end]) ? augment_closed(points) : augment_open(points)
+
+augment_closed(points) = [points[end-1], points..., points[2]]
+
+augment_open(points) = [points[1], points..., points[end]]
