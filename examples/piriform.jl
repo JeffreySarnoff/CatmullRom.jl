f07d348b164c985f800cfe64acea93af19550c38
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-29 07:22:05 -0400

    Update and rename pirform.jl to piriform.jl

---
+++ examples/piriform.jl
@@ -0,0 +1,14 @@
+using CatmullRom, Plots
+b = 1/2
+a = 2*b
+fx(t) = a * cospi(t)^2
+fy(t) = (a^2/b) * cospi(t)^3 * sinpi(t)
+
+xs = [fx(t) for t=0.0:0.02:1.0]
+ys = [fy(t) for t=0.0:0.02:1.0]
+xys = collect(zip(xs,ys))
+
+xys = [xys[end-1], xys..., xys[2]]
+cxs,cys = catmullrom(xys, 7)
+plot(cxs,cys, size=(500,500))
+  
