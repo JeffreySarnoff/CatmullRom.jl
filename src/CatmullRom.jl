b4d896d8672f8e7eba26f4fc13a82a0cc071c7f9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-29 08:18:47 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -6,11 +6,15 @@ This is the form that does not crimp, provides taughtness, and looks good.
 """
 module CatmullRom
 
-export catmullrom,    # populates with points placed between those given
-       extendbounds   # extrapolate just beyond endpoints from adjacent points
+export catmullrom     # populates with points placed between those given
 
 # available as `CatmullRom.extendbounds`, `CatmullRom.thiele3`, etc
 # reflect, linear, quadratic, thiele3, thiele4  # also for interpolation
+
+const Type1 = AbstractArray{AbstractArray{T,1},1} where {T}
+const Type2 = AbstractArray{NTuple{N,T},1} where {N,T}
+const Type3 = NTuple{N, NTuple{M,T}} where {N,M,T}
+const POINTS = Union{Type1, Type2, Type3} where {N,M,T}
 
 include("centripetal/catmullrom.jl")
 include("centripetal/support.jl")
