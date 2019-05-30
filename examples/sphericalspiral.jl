c644d573c488764752953798b1135252a18d16d9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-30 13:26:22 -0400

    Update sphericalspiral.jl

--- examples/sphericalspiral.jl
+++ examples/sphericalspiral.jl
@@ -1,6 +1,13 @@
 http://mathworld.wolfram.com/SphericalSpiral.html
 
 
+julia> x(t) = cos(t) / sqrt(1 + t*t)
+
+julia> y(t) = sin(t) / sqrt(1 + t*t)
+
+julia> z(t) = - t/sqrt(1+t*t)
+            
+            
 julia> ts=collect(range(-4pi,4pi, length=30));
 
 julia> xs=x.(ts);
