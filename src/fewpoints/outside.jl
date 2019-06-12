beb045b75c136833ab3b4a607580d6046c578780
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-12 18:13:03 -0400

    Update outside.jl

--- src/fewpoints/outside.jl
+++ src/fewpoints/outside.jl
@@ -9,8 +9,8 @@ function outside(initialpoints::T, finalpoints::T) where {T}
     nfinals   = length(finalpoints)
     (ninitials > 1 && nfinals > 1) || throw(DomainError("missing points"))
     
-    xinitial = pre()
-    xfinal   = post()
+    xinitial = reflectback(initialpoints[1:2]...,)[1]
+    xfinal   = reflectforward(finalpoints[end-1:end]...,)[1]
     initial = ninitials > 2 ? thiele3(initialpoints[1:3], xinitial) : linear(initialpoints[1:2], xinitial)
     final  = ninitials > 2 ? thiele3(finalpoints[1:3], xfinal) : linear(finalpoints[1:2], xfinal)
 
