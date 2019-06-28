723d85af6bc7a477efe0b214597a4a1c31d7cfc0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-28 16:52:45 -0400

    Update outside.jl

--- src/fewpoints/outside.jl
+++ src/fewpoints/outside.jl
@@ -1,27 +1,29 @@
-function outside(points)
-    initial, final = outside(points[1:4], points[end-3:end])
-    pushfirst!(push!(points, final), initial)
+"""
+    outside(points [; scale=1/16])
+
+Determine the two extremal points (outside of the boundry points)
+used to anchor the sequence of centripetal Catmull-Rom spline spans.
+Incorporate these boundry points in the point sequence.
+"""
+function outside(points::Points; scale=1/16)
+    npoints(points) > 3 || throw(DomainError("4 or more points are required"))
+    firstpoints = points[1:4]
+    lastpoints  = points[end-3:end]
+    initialpoint, finalpoint = pointbefore(firstpoints, scale), pointafter(lastpoints, scale)
+    pushfirst!(push!(points, finalpoint), initialpoint)
     return points
 end
 
-outside(points::Tuple) = outside([points...,])
+function pointbefore(firstpoints::Points, scale)
+    initialx = firstpoints[1][1] + (firstpoints[1][1] - firstpoints[2][1]) * scale
+    initialys = thiele4(firstpoints..., initialx)
+    initialpoint = (initialx, initialys...,)
+    return convert(eltype(firstpoints), initialpoint)
+end
 
-#=
-   Determine two extremal points (outside of the boundry points)
-   for use in anchoring the sequence of centripetal Catmull-Rom
-   spline spans to incorporate the boundry points in the spline.
-=#
-
-function outside(initialpoints::T, finalpoints::T) where {T}
-    ninitials = length(initialpoints)
-    nfinals   = length(finalpoints)
-    (ninitials > 1 && nfinals > 1) || throw(DomainError("missing points"))
-    
-    initialpt = reflectback(initialpoints[1:2]...,)
-    finalpt   = reflectforward(finalpoints[end-1:end]...,)
-    initial = ninitials > 3 ? thiele4(initialpoints[1:4]..., initialpt[1]) : 
-              ninitials > 2 ? thiele3(initialpoints[1:3]..., initialpt[1]) : initialpt
-    final   = nfinals > 3 ? thiele4(finalpoints[1:4]..., finalpt[1]) : 
-              nfinals > 2 ? thiele3(finalpoints[1:3]..., finalpt[1]) : finalpt
-    return initial, final
+function pointafter(lastpoints::Points, scale)
+    finalx = lastpoints[end][1] + (lastpoints[end][1] - lastpoints[end-1][1]) * scale
+    finalys = thiele4(lastpoints..., finalx)
+    finalpoint = (finalx, finalys...,)
+    return convert(eltype(lastpoints), finalpoint)
 end
