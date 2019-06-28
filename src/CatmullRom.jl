c3ec3d3b18795dffd9307ff42a06e6393a3fb0f9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-28 16:54:26 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -17,17 +17,15 @@ using Polynomials: Poly, polyval, polyder, polyint
 #     defines `Points`m `npoints(Points)`, `ndims(Points)`
 include("pointsequences.jl")
 
+# suggest outermost two points for Catmull-Rom spline sequence
+include("fewpoints/outside.jl")
 
 # centripetal Catmull-Rom compution
 include("centripetal/catmullrom.jl")
 include("centripetal/support.jl")
 include("centripetal/arcbased.jl")
 
-
-# suggest outermost two points for Catmull-Rom spline sequence
-include("fewpoints/outside.jl")
-
-# extrapolation using low-order interpolatory fitting
+# extrapolation using low-order interpolatory fits
 include("fewpoints/twopoints.jl")    # linear fit, reflection
 include("fewpoints/threepoints.jl")  # thiele3 rational fit, general quadratic fit
 include("fewpoints/fourpoints.jl")   # thiele4 rational fit (preferred)
