da673cef87847267ffcc05a2c35ab46ac5d76c6c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-28 17:51:05 -0400

    Update support.jl

--- src/centripetal/support.jl
+++ src/centripetal/support.jl
@@ -1,6 +1,9 @@
 isclosed(firstpoint::OnePoint, lastpoint::OnePoint) = firstpoint == lastpoint
 isclosed(points::Points) = isclosed(first(points), last(points))
 
+isopen(firstpoint::OnePoint, lastpoint::OnePoint) = firstpoint != lastpoint
+isopen(points::Points) = isopen(first(points), last(points))
+
 
 augment_points(points) =
     isclosed(points[1], points[end]) ? augment_closed(points) : augment_open(points)
