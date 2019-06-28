12074e4b887ed37420cd954811eb4b2dff9a6c64
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-28 16:04:56 -0400

    Update CatmullRom.jl

--- src/CatmullRom.jl
+++ src/CatmullRom.jl
@@ -30,14 +30,8 @@ using Polynomials: Poly, polyval, polyder, polyint
 # reflect, linear, quadratic, thiele3, thiele4  # also for interpolation
 
 # The sorts of sequences understood to hold point coordinates
-const VecNumVec = AbstractArray{Array{T,1},1} where {T<:Number};
-const VecNumTup = AbstractArray{NTuple{N,T},1} where {N,T<:Number};
-const TupNumTup = NTuple{M,NTuple{N,T}} where {M,N,T<:Number};
-const TupNumVec = NTuple{M,Array{T,1}} where {M,N,T<:Number};
-const VecNumNT = AbstractArray{NamedTuple{S,NTuple{N,T}},1} where {S,N,T<:Number};
-const TupNumNT = NTuple{M,NamedTuple{S,NTuple{N,T}}} where {M,S,N,T<:Number};
-
-const Points = Union{VecNumVec, VecNumTup, TupNumTup, TupNumVec, VecNumNT, TupNumNT};
+#     defines `Points`m `npoints(Points)`, `ndims(Points)`
+include("pointsequences.jl")
 
 include("centripetal/catmullrom.jl")
 include("centripetal/support.jl")
