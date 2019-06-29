0bf2ab83123665a00e3aa9d63162eb21b74d0025
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-29 05:57:20 -0400

    Update arcbased.jl

--- src/centripetal/arcbased.jl
+++ src/centripetal/arcbased.jl
@@ -1,18 +1,99 @@
-#=
-    Given 4 ND points along a centripetal Catmull-Rom span,
-    roughly approximate the arclength of the curvilinear segment
-    that would be determined by the two bounding points
-    and the tangents they determine [the arc between p1 and p2].
-    
-    This well-behaved approximation was developed by Jens Gravesen
+"""
+    catmullrom_points_per_arc(points, n_points_to_realize)
 
+Convert a sequence of CatmullRom points, those given
+as data and those interpolated through the data,
+to a corresponding sequence of point counts,
+each arc having one or more points (the initial boundary
+arc has two or more points).
+"""
+function catmullrom_points_per_arc(points::T, n_points_to_realize::Int) where {T<:Points}
+    normalized_arclengths = catmullrom_normalized_arclengths(points)
+    
+    smallest_arc = minimum(normalized_arclengths)
+    rational_arc = rationalize(Float16(smallest_arc))
+    scalefactor = n_points_to_realize / denominator(rational_arc)
+
+    # scaled_arclengths_are_point_counts counting points on the arc
+    points_per_arc = round.(Int, normalized_arclengths .* scalefactor)
+    return points_per_arc
+end    
+   
+
+"""
+    catmullrom_normalized_arclengths(points)
+
+Convert a sequence of CatmullRom points, those given
+as data and those interpolated through the data,
+to a corresponding sequence of normalized arclengths.
+
+The sum of the normalized arclengths is 1.0.  Each
+arc's approximated length is a portion of unity.
+The most probable arclength, and the mean of these
+normalized arclengths are roughly equal; given by:
+`1//number_of_arcs`.
+
+The way to use normalized arclengths is to multiply
+the fractional lengths by the number of "plottable"
+points (the given data + the interpoint interpolants).
+
+```
+smallest_arc = minimum(normalized_arclengths)
+rational_arc = rationalize(Float16(smallest_arc))
+scalefactor = points_to_realize / denominator(rational_arc)
+
+# scaled_arclengths_are_point_counts counting points on the arc
+points_per_arc = round.(Int, normalized_arclengths .* scalefactor)
+```
+"""
+function catmullrom_normalized_arclengths(points::T) where {T<:Points}
+    arclengths = catmulrom_arclengths(points)
+    sum_of_arcs = sum(arclengths)
+    arclengths[:] = arclengths ./ sum_of_arcs
+    return arclengths
+end
+
+
+"""
+    catmullrom_arclengths(points)
+
+Convert a sequence of CatmullRom points, those given
+as data and those interpolated through the data,
+to a corresponding sequence of arclength approximations.
+"""
+function catmullrom_arclengths(points::T) where {T<:Points}
+    L = float(coordtype(T))
+    n_points = npoints(points)
+    n_arcs = n_points - 2 # first and last points are anchors
+    result = Array{L, 1}(undef, n_arcs)
+
+    for idx = 1:n_points-3
+        arc = catmullrom_approx_arclength(points[idx:idx+3]...,)
+        result[idx] = arc
+    end   
+    return result    
+end
+
+
+"""
+    catmullrom_approx_arclength(pt0, pt1, pt2, pt3)
+
+Given 4 ND points along a centripetal Catmull-Rom span,
+roughly approximate the arclength of the curvilinear segment
+that would be determined by the two bounding points
+and the tangents they determine [the arc between p1 and p2].
+
+This well-behaved approximation was developed by Jens Gravesen
+
+```
     (2*corddist + (n-1)*bezdist)/(n+1), n is degree of the curve
+
     deg=2 --> (2*corddist + bezdist)/(3)
     deg=3 --> (2*corddist + 2*bezdist)/(4) --> (corddist + bezdist)/2
     deg=4 --> (2*corddsit + 3*bezdist)/(5)
-=#
-
-function catmullrom_approx_arclen(p0::T, p1::T, p2::T, p3::T) where {T<:OnePoint}
+```
+"""
+function catmullrom_approx_arclength(p0::T, p1::T, p2::T, p3::T) where {T<:OnePoint}
     b0, b1, b2, b3 = catmullrom_as_bezier(p0, p1, p2, p3)
     cordal_dist = norm(b3 .- b0)
     bezier_dist = norm(b1 .- b0) + norm(b2 .- b1) + norm(b3 .- b2)
