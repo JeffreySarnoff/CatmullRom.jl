6a5eff529f26cdb16d65b8f7c05617be74010777
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-29 05:11:40 -0400

    Update arcbased.jl

--- src/centripetal/arcbased.jl
+++ src/centripetal/arcbased.jl
@@ -12,24 +12,28 @@
     deg=4 --> (2*corddsit + 3*bezdist)/(5)
 =#
 
-function catmullrom_approx_arclen(p0, p1, p2, p3)
-    b0, b1, b2, b3 = catmullrom2bezier(p0, p1, p2, p3)
-    corddist = norm(b3 .- b0)
-    bezdist = norm(b1 .- b0) + norm(b2 .- b1) + norm(b3 .- b2)
-    return (corddist + bezdist) * 0.5
+function catmullrom_approx_arclen(p0::T, p1::T, p2::T, p3::T) where {T<:OnePoint}
+    b0, b1, b2, b3 = catmullrom_as_bezier(p0, p1, p2, p3)
+    cordal_dist = norm(b3 .- b0)
+    bezier_dist = norm(b1 .- b0) + norm(b2 .- b1) + norm(b3 .- b2)
+    
+    return (cordal_dist + bezier_dist) * 0.5
 end
 
-function catmullrom2bezier(p0, p1, p2, p3)
+function catmullrom_as_bezier(p0::T, p1::T, p2::T, p3::T) where {T<:OnePoint}
     b0 = p1
     b3 = p2
+    
     d1 = norm(p1 .- p0); d1a = sqrt(d1)
     d2 = norm(p2 .- p1); d2a = sqrt(d2)
     d3 = norm(p3 .- p2); d3a = sqrt(d3)
+    
     b1n = @. (d1 * p2) - (d2 * p0) + ((2*d1 + 3*d1a*d2a+d2) * p1)
     b1d = 3*d1a*(d1a+d2a)
     b1 = b1n ./ b1d
     b2n = @. (d3 * p1) - (d2 * p3) + ((2*d3 + 3*d3a*d2a+d2) * p2)
     b2d = 3*d3a*(d3a+d2a)
     b2 = b2n ./ b2d
+    
     return b0, b1, b2, b3
 end
