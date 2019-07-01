55743682feb64213d54edb3746e9f43ea46cb27d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-30 22:32:23 -0400

    Update sphericalspiral.jl

--- examples/sphericalspiral.jl
+++ examples/sphericalspiral.jl
@@ -14,8 +14,21 @@ zs = z.(ts);
 
 plot(zs,ys,xs,linecolor=:navy)
 
+#=
 zyxs = collect(zip(zs,ys,xs));
-zz,yy,xx = catmullrom(zyxs, 32);
+zs,ys,xs = catmullrom(collect(zip(zs,ys,xs)), 32, iterator=false);
+zz,yy,xx = catmullrom(zyxs, 32, iterator=false);
 
 plot(zz,yy,xx)
+=#
+
+CatmullRom.catmullrom(a::Base.Iterators.Flatten{Array{Array{T,1},1}}, n::Int; iterator=false) where {T} =
+    catmullrom(collect(a), n, iterator=iterator)
+
+CatmullRom.catmullrom(a::Base.Iterators.Zip{Tuple{Array{T,1},Array{T,1},Array{T,1}}}, n::Int; iterator=false) where {T} =
+    catmullrom(collect(a), n, iterator=iterator)
+
+zs,ys,xs = catmullrom(zip(zs,ys,xs), 32, iterator=false);
+plot!(zs,ys,xs,linecolor=:navy)
+
 ```
