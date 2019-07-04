35c192ecb64b01466b91ccb62b0d58ec8cc172c8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 11:56:47 -0400

    Update piriform.jl

--- examples/piriform.jl
+++ examples/piriform.jl
@@ -7,13 +7,8 @@ fy(t) = (a^2/b) * cospi(t)^3 * sinpi(t);
 
 xs = [fx(t) for t=range(0.0, stop=1.0, length=16)];
 ys = [fy(t) for t=range(0.0, stop=1.0, length=16)];
-xys = collect(zip(xs,ys));
 
-if isapprox(xs[1], xs[end])
-   xys = [xys[end-1], xys..., xys[2]]
-end;
+cxs, cys = catmullrom(xs, ys, 16);
 
-cxs,cys = catmullrom(xys, 16);
-
-plot(xs, ys, linecolor=:navy, size=(600,600), legend=nothing)
-plot!(cxs,cys, linecolor=:green, size=(600,600), legend=nothing) 
+plot(xs, ys, linecolor=:navy, size=(600,600), legend=nothing, xaxis=nothing, yaxis=nothing)
+plot!(cxs,cys, linecolor=:green, size=(600,600), legend=nothing, xaxis=nothing, yaxis=nothing) 
