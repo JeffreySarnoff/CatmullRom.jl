87f4842df74a9dc07d78c07f8933dcd3a006b2d5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-04 11:52:40 -0400

    Update circle.jl

--- examples/circle.jl
+++ examples/circle.jl
@@ -11,14 +11,8 @@ fy(t) = sinpi(t);
 
 xs = [fx(t) for t=range(0.0, stop=2.0, length=17)];
 ys = [fy(t) for t=range(0.0, stop=2.0, length=17)];
-xys = collect(zip(xs,ys));
 
-if isapprox(xs[1], xs[end])
-   xys = [xys[end-1], xys..., xys[2]]
-   cxs,cys = catmullrom(xys, 16, extend=false);
-else
-   cxs,cys = catmullrom(xys, 16);
-end;
+cxs,cys = catmullrom(xs, ys, 16);
 
 plot(xs, ys, linecolor=:black, size=(600,600), legend=nothing, xaxis=nothing)
 plot!(cxs,cys, linecolor=:blue, size=(600,600), legend=nothing, yaxis=nothing) 
