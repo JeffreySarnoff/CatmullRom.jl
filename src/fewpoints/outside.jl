a10c246e891e53936cf53291a075bb100afcecb2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-09 03:34:32 -0400

    Update outside.jl

--- src/fewpoints/outside.jl
+++ src/fewpoints/outside.jl
@@ -19,10 +19,11 @@ used to anchor the sequence of centripetal Catmull-Rom spline spans.
 Incorporate these boundary points in the point sequence.
 """
 function outside(points::Points, scale::F) where {F<:AbstractFloat}
+    extendedpoints = copy(points)
     initialpoint = pointbefore(points[1:4], scale)
     finalpoint   = pointafter(points[end-3:end], scale)
-    pushfirst!(push!(points, finalpoint), initialpoint)
-    return points
+    pushfirst!(push!(extendedpoints, finalpoint), initialpoint)
+    return extendedpoints
 end
 
 function pointbefore(firstpoints::Points, scale)
