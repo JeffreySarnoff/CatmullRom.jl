ae1e6ecf59edb0126f7087e6921e052b910cda1a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-09 19:57:15 -0400

    Create pointtypes.jl

---
+++ src/presence/pointtypes.jl
@@ -0,0 +1,15 @@
+# The sorts of structures understood to hold the coordinates of a point
+const NumVec = Array{T,1} where {T<:Number};
+const NumTup = NTuple{N,T} where {N,T<:Number};
+const NumNT = NamedTuple{S,NTuple{N,T}} where {S,N,T<:Number};
+const OnePoint = Union{NumVec, NumTup, NumNT}
+
+# The sorts of sequences understood to hold point coordinates
+const VecNumVec = AbstractArray{Array{T,1},1} where {T<:Number};
+const VecNumTup = AbstractArray{NTuple{N,T},1} where {N,T<:Number};
+const TupNumTup = NTuple{M,NTuple{N,T}} where {M,N,T<:Number};
+const TupNumVec = NTuple{M,Array{T,1}} where {M,N,T<:Number};
+const VecNumNT = AbstractArray{NamedTuple{S,NTuple{N,T}},1} where {S,N,T<:Number};
+const TupNumNT = NTuple{M,NamedTuple{S,NTuple{N,T}}} where {M,S,N,T<:Number};
+
+const Points = Union{VecNumVec, VecNumTup, TupNumTup, TupNumVec, VecNumNT, TupNumNT};
