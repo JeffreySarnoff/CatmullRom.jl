8da9359072846e344210c68838c49713afe07f36
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-09 11:41:48 -0400

    Update teardrop.jl

--- examples/teardrop.jl
+++ examples/teardrop.jl
@@ -1,3 +1,5 @@
+using CatmullRom, Plots
+
 x(t) = cos(t)
 y(t,m=4) = sin(t) * (sin(t/2)^m) # m=0..7
 
@@ -6,8 +8,15 @@ given = collect(range(0.0, 2.0*pi, length=15));
 xs = x.(given);
 ys = y.(given, 5);
 
-plot(xs, ys)
-
 cmpts = catmullrom(xs,ys,n_between);
 cxs,cys=cmpts;
-plot!(cxs,cys)
+
+plot(xs, ys, linecolor=:lightgreen, linewidth=9, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
+plot!(cxs, cys, linecolor=:black, linewidth=2, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
+
+
+crpts = catmullrombyarc(collect(zip(xs,ys)), arcpoints_min=8, arcpoints_max=64);
+cxs,cys=crpts;
+
+plot(xs, ys, linecolor=:lightgreen, linewidth=9, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
+plot!(cxs, cys, linecolor=:black, linewidth=2, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
