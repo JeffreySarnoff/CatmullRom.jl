e4e7d80e7afe0452a33ce5744b8d9d8d66cd56de
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-10 09:49:03 -0400

    Update teardrop.jl

--- examples/teardrop.jl
+++ examples/teardrop.jl
@@ -4,7 +4,7 @@ x(t) = cos(t)
 y(t,m=4) = sin(t) * (sin(t/2)^m) # m=0..7
 
 n_between = 24
-given = collect(range(0.0, 2.0*pi, length=15));
+given = collect(range(0.0, length=15, stop=2.0*pi));
 xs = x.(given);
 ys = y.(given, 5);
 
