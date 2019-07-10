2bcbb4e5609618cf1fee24dc2248ce0bb8d0c417
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-10 09:33:42 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using CatmullRom
 using Test
 
 @testset "closed curve interpositions" begin
-  xs = [sinpi(t) for t=range(0.0, 2.0, length=5)];
-  ys = [cospi(t) for t=range(0.0, 2.0, length=5)];
+  xs = [sinpi(t) for t=range(0.0, stop=2.0, length=5)];
+  ys = [cospi(t) for t=range(0.0, stop=2.0, length=5)];
   points = collect(zip(xs,ys));
   cr_xys = catmullrom(points, 2)
   @test length(cr_xys[1]) == length(xs) + 2*(length(xs)-1)
