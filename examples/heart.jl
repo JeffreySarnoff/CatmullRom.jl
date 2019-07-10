404aa3c6e70dd2e18553bcb9fbbb615058d30a2f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-10 01:35:45 -0400

    Update heart.jl

--- examples/heart.jl
+++ examples/heart.jl
@@ -1,3 +1,5 @@
+```julia
+using CatmullRom, Plots
 
 xt(t) = 16 * sin(t)^3
 yt(t) = 13 * cos(t) - 5 * cos(2*t) - 2 * cos(3*t) - cos(4*t)
@@ -19,24 +21,4 @@ cxs,cys=crpts;
 
 plot(xs, ys, linecolor=:lightgreen, linewidth=7, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
 plot!(cxs, cys, linecolor=:black, linewidth=2, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
-
-#=
-
-julia> given = collect(range(0.0,2.0*pi, length=15));
-
-julia> xs=x.(given);ys=y.(given);
-
-julia> plot(xs,ys)
-
-julia> cmpts = catmullrom(xs,ys,n_between);
-
-julia> cxs,cys=cmpts;
-
-julia> plot(cxs,cys)
-
-julia> cmpts = catmullrom(xs,ys,32);
-
-julia> cxs,cys=cmpts;
-
-julia> plot(cxs,cys)
-=#
+```
