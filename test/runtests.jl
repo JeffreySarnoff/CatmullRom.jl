982462287dea454d54bf39b339c527a47e614e09
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-10 05:16:25 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,25 @@ using Test
   @test length(points) == length(xs) + 2
 end
 
+#=
+    example from
+    Block-based Thiele-like blending rational interpolation
+    Qian-Jin Zhao, Jieqing Tan
+    Journal of Computational and Applied Mathematics
+    https://doi.org/10.1016/j.cam.2005.03.089
+=#
+@testset "interpolation" begin
+  xs=[0.0, 1.0, 2.0,  3.0,  4.0, 5.0]
+  ys=[0.0, 1.0, 4.0, -1.0, -2.0, 2.0]
+  points = collect(zip(xs,ys))
+  @test CatmullRom.thiele4(points[1:4]..., 6.0)[1] == 2.0 + 2/3
+  @test CatmullRom.thiele4(points[2:5]..., 1.0)[1] == 1.0
+  @test CatmullRom.quadratic(points[3:5]..., 1.5)[1] == 8.0
+end
+
+@testset "approximate arclengths"
+  xs=[0.0, 1.0, 2.0,  3.0,  4.0, 5.0]
+  ys=[0.0, 1.0, 4.0, -1.0, -2.0, 2.0]
+  points = collect(zip(xs,ys))
+  @test length(points) - 2*2 + 1 == length(CatmullRom.approx_catmullrom_arclengths(points))  
+end
