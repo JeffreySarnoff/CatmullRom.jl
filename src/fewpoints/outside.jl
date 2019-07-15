c4ce88669b3153acc47c405f38fefaeccd59247b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-15 11:56:32 -0400

    Update outside.jl

--- src/fewpoints/outside.jl
+++ src/fewpoints/outside.jl
@@ -1,11 +1,11 @@
-function pointbefore(firstpoints::Points, scale=ReflectionScale)
+function pointbefore(firstpoints::P, scale=ReflectionScale) where P
     initialx = reflectback(first.(firstpoints[1:2])..., scale=scale)
     initialys = thiele4(firstpoints..., initialx)
     initialpoint = (initialx, initialys...,)
     return convert(eltype(firstpoints), initialpoint)
 end
 
-function pointafter(lastpoints::Points, scale=ReflectionScale)
+function pointafter(lastpoints::P, scale=ReflectionScale) where P
     finalx = reflectforward(first.(lastpoints[end-1:end])..., scale=scale)
     finalys = thiele4(lastpoints..., finalx)
     finalpoint = (finalx, finalys...,)
