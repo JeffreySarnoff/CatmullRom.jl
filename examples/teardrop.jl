91d48d6bd05486cd2171fbde24f1be25de48b9a3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-15 08:27:18 -0400

    Update teardrop.jl

--- examples/teardrop.jl
+++ examples/teardrop.jl
@@ -8,15 +8,19 @@ given = collect(range(0.0, length=15, stop=2.0*pi));
 xs = x.(given);
 ys = y.(given, 5);
 
-cmpts = catmullrom(xs,ys,n_between);
-cxs,cys=cmpts;
+points = collect(zip(xs,ys)
+  
+crpoints = catmullrom(points), n_between); #
+cxs, cys = crpoints;                       # and
+cxs, cys = catmullrom(points), n_between); # or
+
 
 plot(xs, ys, linecolor=:lightgreen, linewidth=9, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
 plot!(cxs, cys, linecolor=:black, linewidth=2, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
 
 
-crpts = catmullrom_byarc(collect(zip(xs,ys)), arcpoints_min=6, arcpoints_max=16);
-cxs,cys=crpts;
+crpoints = catmullrom_byarc(collect(zip(xs,ys)), arcpoints_min=6, arcpoints_max=16);
+cxs, cys = crpoints;
 
 plot(xs, ys, linecolor=:lightgreen, linewidth=10, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
 plot!(cxs, cys, linecolor=:black, linewidth=2, size=(500,500), xaxis=nothing, yaxis=nothing, legend=nothing)
