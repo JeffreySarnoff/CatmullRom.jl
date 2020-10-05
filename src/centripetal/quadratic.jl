82de9bbb1a2999cae06b723227e3a63141d52ca5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-05 02:26:29 -0400

    Update quadratic.jl

--- src/centripetal/quadratic.jl
+++ src/centripetal/quadratic.jl
@@ -23,3 +23,26 @@ function quadraticBezierLength2d(p0, p1, p2)
     result = (r1 + r2 + r3) / r4
     return result # (A32 * Sabc + A2 * B * (Sabc - C2) + (4 * C * A - B * B) * log((2 * A2 + BA + Sabc) / (BA + C2))) / (4 * A32)
 end
+
+function quadraticBezierLength(p0, p1, p2)
+    a = p0 .- 2 .* p1 .+ p2
+    b = 2 .* p1 .- 2 .* p0
+    A = 4 * sum(a .* a)
+    B = 4 * sum(a .* b)
+    C = sum(b .* b)
+
+    Sabc = 2 * sqrt(A+B+C)
+    A2 = sqrt(A)
+    A32 = 2 * A * A2
+    C2 = 2 * sqrt(C)
+    BA = B / A2
+
+    r1 = A32 * Sabc
+    r2 = A2 * B * (Sabc - C2)
+    r3a = (4 * C * A - B * B)
+    r3b = (2 * A2 + BA + Sabc) / (BA + C2)
+    r3 = log(r3a * r3b)
+    r4 = 4 * A32
+    result = (r1 + r2 + r3) / r4
+    return result # (A32 * Sabc + A2 * B * (Sabc - C2) + (4 * C * A - B * B) * log((2 * A2 + BA + Sabc) / (BA + C2))) / (4 * A32)
+end
