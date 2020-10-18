60e398493e5f83a87b699ec7128833a3e871e30a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-18 04:49:49 -0400

    Update and rename angle.jl to distance.jl

--- src/support/distance.jl
+++ src/support/distance.jl
@@ -1,3 +1,12 @@
+root4(x) = sqrt(sqrt(x))
+
+distancesquared(a::T, b::T) where {T} = sum(subsq(a, b))
+
+subsq(a::NTuple{N,T}, b::NTuple{N,T}) where {N,T} =
+    (((a[i] - b[i])^2 for i=1:N)...,)
+
+subsq(a::T, b::T) where {S,T<:AbstractVector{S}} =
+    (((a[i] - b[i])^2 for i=1:min(length(a),length(b)))...,)
 
 #=
   relatively fast determination of angular separation
