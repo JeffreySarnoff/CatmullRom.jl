501f778193fcd3646d24c6bae1b7021b69348130
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-18 05:01:49 -0400

    Update distance.jl

--- src/support/distance.jl
+++ src/support/distance.jl
@@ -1,6 +1,13 @@
 root4(x) = sqrt(sqrt(x))
 
-distancesquared(a::T, b::T) where {T} = sum(subsq(a, b))
+distancesquared(x::T, y::T) where {T<:Number} = x*x + y*y
+distancesquared(x::T, y::T, z::T) where {T<:Number} = x*x + y*y + z*z
+distancesquared(w::T, x::T, y::T, z::T) where {T<:Number} = w*w + x*x + y*y + z*z
+distance(x::T, y::T) where {T<:Number} = sqrt(distancesquared(x,y))
+distance(x::T, y::T, z::T) where {T<:Number} = sqrt(distancesquared(x,y,z))
+distance(w::T, x::T, y::T, z::T) where {T<:Number} = sqrt(distancesquared(w,x,y,z))
+
+distancesquared(a::A, b::A) where {N,T,S,A<:Union{NTuple{N,T},AbstractVector{S}}} = sum(subsq(a, b))
 
 subsq(a::NTuple{N,T}, b::NTuple{N,T}) where {N,T} =
     (((a[i] - b[i])^2 for i=1:N)...,)
